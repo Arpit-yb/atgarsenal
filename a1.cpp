27537c5b0e4e417b363a7e5052e5273908ca63a8
Arpit472 <54769894+Arpit472@users.noreply.github.com>
2019-10-25 17:17:32 +0530

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct store
+{
+	int base;
+	int ans;
+	string num;
+};
+int findnum(int base,string num)
+{
+	int r;
+	for(int i=0;num[i]!=NULL;i++)
+	{
+		
+	}
+}
+int main()
+{
+		 int t;
+		cin>>t;
+		while(t!=0)
+		{
+		int n;
+		cin>>n;
+		struct store a[n];
+		int answer=-1;
+		for(int i=0;i<n;i++)
+		{
+			int b;
+			string s;
+			cin>>b>>s;
+			a[i].base=b;
+			a[i].num=s;
+			if(b!=-1 && answer==-1)
+				{
+					a[i].ans=findnum(b,s);
+				}
+				
+		}
+				
+		t--;
+	}}
